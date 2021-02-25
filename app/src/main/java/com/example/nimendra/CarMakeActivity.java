0012c4eda9feec7d5762c0fa9b30fedf2b8dbbe9
NimendraK <nimendra.2019264@iit.ac.lk>
2021-02-25 23:04:25 +0530

    working on car make

---
+++ app/src/main/java/com/example/nimendra/CarMakeActivity.java
@@ -0,0 +1,46 @@
+package com.example.nimendra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CarMakeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_make);
+
+        // create the spinner
+        Spinner spinner = findViewById(R.id.car_make_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array,
+                R.layout.spinner_color_layout);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
--- app/src/main/java/com/example/nimendra/MainActivity.java
+++ app/src/main/java/com/example/nimendra/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.nimendra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Class name for Log tag
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // Unique tag required for the intent extra
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchCarMake(View view) {
+        Log.d(LOG_TAG, "CarMakeBtn clicked!");
+        Intent intent = new Intent(this, CarMakeActivity.class);
+        startActivity(intent);
     }
 }
